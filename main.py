212b5eb2b047b613ecd24ee99d692125839ab6fb
Olin <olin.goog@gmail.com>
2026-05-06 21:33:36 -0400

    ready for proxy server

--- main.py
+++ main.py
@@ -5,20 +5,20 @@ import auth
 
 app = Flask(__name__)
 
-@app.get("/api/nodes")
+@app.get("/vlanet/api/nodes")
 def get_nodes():
     auth.verify_key()
     return jsonify(db_api.get_nodes())
 
-@app.get("/api/edges")
+@app.get("/vlanet/api/edges")
 def get_edges():
     auth.verify_key()
     return jsonify(db_api.get_edges())
 
-@app.get("/api/graph")
+@app.get("/vlanet/api/graph")
 def get_graph():
     auth.verify_key()
     return jsonify(db_api.get_graph())
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=80)
+    app.run(host="0.0.0.0", port=8080)
